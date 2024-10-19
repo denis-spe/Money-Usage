46c8d12c7aa79aa71a443fe279ba41756be25429
Mutesasira Denis <den@Mutesasiras-MBP.lan>
2024-10-19 21:27:39 +0200

    Changed the top bar button border

--- app/src/main/java/com/example/moneyusage/frontend/components/TopAppButtons.kt
+++ app/src/main/java/com/example/moneyusage/frontend/components/TopAppButtons.kt
@@ -1,12 +1,17 @@
 package com.example.moneyusage.frontend.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.moneyusage.R
 import com.example.moneyusage.frontend.dataclasses.TopAppButtonArgs
 import com.example.moneyusage.frontend.dataclasses.TopAppButtonColors
 
@@ -15,6 +20,9 @@ fun TopAppButton(
     topAppButtonArgs: TopAppButtonArgs,
     selectedButtonBackground: (label: String) -> TopAppButtonColors
 ){
+    val interactiveClickColor = colorResource(R.color.interactiveClickColor)
+    val textColor = selectedButtonBackground(topAppButtonArgs.label).textColor
+
     Button(
         onClick = {
             topAppButtonArgs.selectedButton.value = topAppButtonArgs.label
@@ -22,14 +30,14 @@ fun TopAppButton(
         },
         colors = ButtonDefaults.buttonColors(
             containerColor = selectedButtonBackground(topAppButtonArgs.label).background,
-        )
+        ),
+        border = BorderStroke(1.dp, interactiveClickColor)
     ) {
         Text(
             topAppButtonArgs.label,
             fontSize = 12.sp,
             fontWeight = FontWeight.ExtraBold,
-            color = selectedButtonBackground(topAppButtonArgs.label).
-                textColor
+            color = textColor
         )
     }
 }
